2c3798cf6ca6f7c7a80b74e7b7e138f22a58de4a
Zephyros <sillybreeze@gmail.com>
2018-02-14 23:23:01 +0800

    new project

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
